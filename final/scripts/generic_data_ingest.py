32f270f9c15c03aac6f3aa7417855935d36edc28
m-wessler <m.wessler@utah.edu>
2021-04-27 09:13:27 -0600

    Exited PhD - Compiled final scripts and docs. Working SVR SLR model ready to go, needs to be applied to a forecast model. All the resources should be here. Create new repo for future work. - MW

---
+++ final/scripts/generic_data_ingest.py
@@ -0,0 +1,103 @@
+import sys
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+from glob import glob
+from os.path import isfile
+from datetime import datetime, timedelta
+from subprocess import Popen, PIPE
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from ingest_config import *
+
+site = sys.argv[1]
+finfo = file_info[site]
+
+f = glob(obdir + '%s*.csv'%site)[0]
+
+data_raw = pd.read_csv(f, low_memory=False)
+
+for old, new, unit in zip(finfo['header'], finfo['rename'], finfo['units']):
+    if new is not None:
+        new = new+'_%s'%unit if unit is not None else new
+        data_raw = data_raw.rename(columns={old:new})
+        
+        if unit is not None:
+            data_raw[new] = data_raw[new].astype(np.float32)
+            
+    else:
+        data_raw = data_raw.drop(columns=old)
+                
+if finfo['tfix'] is not None:
+    data_raw['datetime'] = [datetime.strptime(t, finfo['tfmt'])+timedelta(hours=finfo['tfix']) for t in data_raw['date']]
+    
+elif len(finfo['tfmt']) == 2:
+    data_raw['time'] = data_raw['time'].astype(str)
+    tfmt = ' '.join(finfo['tfmt'])
+    data_raw['datetime'] = [datetime.strptime(' '.join(dt), tfmt) for dt in zip(data_raw['date'], data_raw['time'])]
+    data_raw['datetime'] = [d+timedelta(minutes=60-d.minute) 
+                            if d.minute > 30 else d-timedelta(minutes=d.minute) for d in data_raw['datetime']]
+    
+elif len(finfo['tfmt']) == 1:
+    data_raw['datetime'] = [datetime.strptime(str(t), finfo['tfmt'][0]) for t in data_raw['datetime']]
+
+# If we are going to attempt to assign tzinfo and parse ST vs DT, do it here and now
+# For now this is timezone-naive
+# Convert to UTC
+data_raw['datetime_utc'] = data_raw['datetime'] + timedelta(hours=finfo['tzinfo'])
+
+# Clean things up
+data_raw = data_raw.set_index('datetime_utc')
+data_raw = data_raw.sort_index()
+data_raw = data_raw.drop(columns=[k for k in data_raw.keys() if (('date' in k)|('time' in k))])
+    
+in_mm = 25.4
+cm_mm = 10
+m_mm = 1000
+
+for k in data_raw.keys():
+    
+    data_raw[k] = data_raw[k].replace(-999, np.nan)
+    
+    if k[-3:] == '_in':
+        data_raw[k] *= in_mm
+        data_raw = data_raw.rename(columns={k:k.replace('_in', '_mm')}).round(2)
+    elif k[-3:] == '_cm':
+        data_raw[k] *= cm_mm
+        data_raw = data_raw.rename(columns={k:k.replace('_cm', '_mm')}).round(2)
+    elif k[-2:] == '_m':
+        data_raw[k] *= m_mm
+        data_raw = data_raw.rename(columns={k:k.replace('_m', '_mm')}).round(2)
+    elif k[-5:] == '_degF':
+        data_raw[k] = (data_raw[k] - 32) * (5 / 9)
+        data_raw = data_raw.rename(columns={k:k.replace('_degF', '_degC')}).round(2)
+        
+intervals = [k.split('_')[0].replace('snow', '') for k in data_raw.keys() if ('snow' in k)]
+for i in intervals:
+    data_raw.insert(0, 'slr'+i, np.round(data_raw['snow%s_mm'%i]/data_raw['swe%s_mm'%i], 2))
+    data_raw['slr'+i][np.isinf(data_raw['slr'+i])] = np.nan
+
+# Ensure data is typecast properly
+data_raw = data_raw.fillna(np.nan)
+for k in data_raw.keys():
+    try:
+        data_raw[k] = data_raw[k].astype(np.float32)
+    except:
+        pass
+    
+data_all = data_raw
+
+time_shift = finfo['tshift'] if finfo['tshift'] is not None else 0
+
+if 'precip_periods' not in data_all.keys():
+    data_all['precip_periods'] = np.full(data_all.shape[0], fill_value='[]')
+    
+data_save = data_all
+
+data_save_file = f.split('/')[-1].replace('.csv', '.pd')
+print('Saving %s'%data_save_file)
+
+data_save.to_pickle(obdir + 'clean/' + data_save_file)
---
+++ final/scripts/pair_era5profiles.py
@@ -0,0 +1,386 @@
+import os
+import sys
+import gc
+import numpy as np
+import pandas as pd
+import xarray as xr
+import metpy.constants as mpconsts
+
+from glob import glob
+from os.path import isfile
+from functools import reduce
+from datetime import datetime, timedelta
+
+from sklearn.metrics import r2_score
+from scipy.stats import gaussian_kde, skew
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+obdir = '/uufs/chpc.utah.edu/common/home/steenburgh-group10/mewessler/observations/'
+profdir = '/uufs/chpc.utah.edu/common/home/steenburgh-group10/mewessler/era5/profiles/'
+figdir = '/uufs/chpc.utah.edu/common/home/steenburgh-group10/mewessler/output/slr_figures/'
+
+site = sys.argv[1]
+
+flist = glob(obdir + 'clean/*.pd')
+meta_file = glob(obdir + '*Metadata*.xlsx')[0]
+metadata = pd.read_excel(meta_file)
+
+# Temp fix to use CLNX instead of CLN data
+metadata['code'] = metadata['code'].replace('CLN', 'CLNX')
+metadata = metadata.set_index('code').loc[site]
+
+# Determine the lat, lon of the site from the metadata
+site_lat, site_lon = metadata['lat'], metadata['lon']
+site_elev = metadata['elevation_m']
+
+# Import the era5_orog file to check the lat/lon grid
+era_dir = '/uufs/chpc.utah.edu/common/home/steenburgh-group10/mewessler/era5/'
+era5_orog_file = era_dir + 'era5_orog.nc'
+era5_orog = xr.open_dataset(era5_orog_file)['z'].isel(time=0)
+era5_orog = era5_orog.rename({'latitude':'lat', 'longitude':'lon'})
+era5_lat, era5_lon = np.meshgrid(era5_orog['lat'], era5_orog['lon'])
+
+# Find the index of the correct lat lon
+idx1d = (np.abs(era5_lon - site_lon) + np.abs(era5_lat - site_lat))
+idx = np.unravel_index(np.argmin(idx1d, axis=None), idx1d.shape)
+
+# Subset and convert gpm to m
+era5_g = 9.80665
+era5_orog = era5_orog.isel(lat=idx[1], lon=idx[0])/era5_g
+
+era5_lat, era5_lon = era5_orog['lat'].values, era5_orog['lon'].values
+era5_elev = era5_orog.values
+
+print('ERA5 Profile:\n%.2f %.2f %.2f\n'%(era5_lat, era5_lon, era5_elev))
+
+era5_prof_file = 'era5prof_{:.2f}N_{:.2f}W.nc'.format(
+    era5_orog['lat'].values, abs(era5_orog['lon'].values))
+
+if isfile(era_dir + 'profiles/' + era5_prof_file):
+    era5_prof = xr.open_dataset(era_dir + 'profiles/' + era5_prof_file).load()
+    era5_prof = era5_prof.dropna(dim='time')
+    # print(era5_prof)
+    
+else:
+    print('ERA5 Profile Missing:\n%s'%era5_prof_file)
+
+try:
+    obs = pd.read_pickle(glob(obdir + 'clean/%s*.pd'%site)[0]).reset_index().rename(
+        columns={'index':'time'}).set_index('time')
+except:
+    obs = pd.read_pickle(glob(obdir + 'clean/%s*.pd'%site)[0]).reset_index().rename(
+        columns={'datetime_utc':'time'}).set_index('time')
+
+obs = obs[obs.index > datetime(1980, 8, 1)]
+obs = obs.dropna(how='all')
+
+# plt.figure(facecolor='w')
+# plt.hist([t.hour for t in obs.index], bins=np.arange(0, 24, 1))
+# plt.grid()
+# plt.title('Hour of Observation')
+# plt.show()
+
+intervals = [int(k.replace('slr', '')) for k in obs if 'slr' in k]
+
+### Prune observations based on minimum qpf, snow, and max T 
+try:
+    obs = obs[((obs['swe12_mm'] > 1.4) & (obs['snow12_mm'] >= 25.5))]
+except:
+    print('24h Data Used to Constrain SWE/SNOW')
+    obs = obs[((obs['swe24_mm'] > 2.8) & (obs['snow24_mm'] >= 51.0))]
+else:
+    print('12h Data Used to Constrain SWE/SNOW')
+
+# try:
+#     obs = obs.drop_duplicates()
+# except:
+#     pass
+# else:
+#     pass
+#     #print(obs.shape)
+
+obs = obs.iloc[np.where(np.array([len(pp) for pp in obs['precip_periods']]) > 0)[0]]
+
+### Investigate the data with a few plots
+ycolors = ("#704D9E","#704D9E","#7A4EA0","#824EA3","#8B4FA4","#9350A6","#9B51A7","#A253A8","#AA54A9",
+           "#B156A9","#B758A9","#BE5AA9","#C45DA8","#CA60A7","#CF63A6","#D566A4","#DA6AA3","#DE6DA1",
+           "#E3719E","#E7759C","#EB7A99","#EE7F96","#F18493","#F28A90","#F48F8D","#F5958B","#F79A88",
+           "#F7A086", "#F8A684","#F9AB83","#F9B182","#F9B681","#F9BC81","#F9C182","#F8C783","#F8CC85",
+           "#F7D288","#F6D78B","#F5DC8F","#F4E294","#F3E79A")
+
+binwidth = 2.5
+
+for k in [k for k in obs if 'slr' in k]:
+
+    fig, axs = plt.subplots(2, 3, figsize=(24, 14), facecolor='w')
+    axs = axs.flatten()
+    ax1, ax2, ax3, ax4, ax5, ax6 = axs
+    
+    histargs = {'width':1, 'color':'0.5', 'edgecolor':'k', 'zorder':10}
+    vlineargs = {'linewidth':3, 'linestyle':'--', 'zorder':11}
+    xyargs = {'s':150, 'marker':'+', 'linewidth':1.5, 'zorder':10}
+    
+    v1, b1 = np.histogram(obs[k].values, bins=np.arange(0, 50+1, binwidth))    
+    ax1.bar(np.arange(.5, len(v1), 1), v1/sum(v1), label=k.upper(), **histargs)
+    vmean = obs[k].mean()
+    ax1.axvline(vmean/binwidth, c='green', label='Mean: %.2f'%vmean, **vlineargs)
+    vmed = obs[k].median()
+    ax1.axvline(vmed/binwidth, c='orange', label='Median: %.2f'%vmed, **vlineargs)
+    
+    ax1.set_xticks(np.arange(len(b1)))
+    ax1.set_xticklabels(b1, rotation=60)
+    ax1.set_xlabel(k.upper())
+    ax1.set_ylabel('FREQ_NORM')
+    
+    years = np.unique([t.year for t in obs.index])
+    nyears = len(years)
+    ygrays = np.linspace(.8, .4, nyears).astype(str)
+    
+    colid = np.linspace(0, len(ycolors)-1, nyears, dtype=int)
+    
+    for i, year in enumerate(np.unique([t.year for t in obs.index])):
+        
+        label = year if i==0 or i==nyears-1 else None
+        yobs = obs[k].iloc[[i for i, t in enumerate(obs.index) if t.year == year]]
+
+        try:
+            n, x = np.histogram(yobs.dropna(), bins=np.arange(0, 50+1, 0.1))
+            ax2.plot(x, gaussian_kde(yobs.dropna())(x), color=ycolors[colid[i]], linewidth=2, label=label)
+
+            n, x = np.histogram(yobs.dropna(), bins=np.arange(0, 50+1, 0.1))
+            ax3.plot(x, np.cumsum(gaussian_kde(yobs.dropna())(x))/10, color=ycolors[colid[i]], linewidth=2, label=label)
+        except:
+            pass
+            #raise
+        
+        ax5.scatter(year, yobs.dropna().mean(), c='orange', **xyargs)
+        ax5.scatter(year, yobs.dropna().median(), c='green', **xyargs)        
+        ax6.scatter(year, np.std(yobs.dropna()), c='purple', **xyargs)
+        ax6.scatter(year, skew(yobs.dropna()), c='blue', **xyargs)
+        
+    ax5.scatter(year, -1, c='orange', label='Mean', **xyargs)
+    ax5.scatter(year, -1, c='green', label='Median', **xyargs)        
+    ax6.scatter(year, -1, c='purple', label='Standard Deviation', **xyargs)
+    ax6.scatter(year, -1, c='blue', label='Skewness', **xyargs)
+    
+    try:
+        n, x = np.histogram(yobs, bins=np.arange(0, 50+1, 0.1))
+        ax2.plot(x, gaussian_kde(obs[k].dropna())(x), 'k--', linewidth=2, label='All')
+        n, x = np.histogram(yobs, bins=np.arange(0, 50+1, 0.1))
+        ax3.plot(x, np.cumsum(gaussian_kde(obs[k].dropna())(x))/10, 'k--', linewidth=2, label='All')
+    except:
+        pass
+        #raise
+    
+    mobs = []
+    for i, month in enumerate([10, 11, 12, 1, 2, 3, 4, 5]):
+        _mobs = obs[k].iloc[[i for i, t in enumerate(obs.index) if t.month == month]]
+        _mobs = _mobs[~np.isnan(_mobs)]
+        mobs.append(_mobs)
+        
+    bp = ax4.boxplot(mobs, showfliers=False, patch_artist=True, zorder=15)
+    [[box.set(linewidth=2, color='k', alpha=0.8) for box in bp[k]] 
+         for k in ['boxes', 'whiskers', 'medians', 'means', 'caps']]
+    [patch.set_facecolor('0.5') for patch in bp['boxes']]
+    
+    ax4.axhline(vmean, c='green', label='Mean: %.2f'%vmean, **vlineargs)
+    ax4.axhline(vmed, c='orange', label='Median: %.2f'%vmed, **vlineargs)
+    
+    ax4.set_xlim(right=9)
+    ax4.set_ylim([0, 50])
+    ax4.set_xticklabels(['Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May'])
+    
+    ax5.set_ylim(top=30)
+    for ax in [ax5, ax6]:
+        ax.set_xlim([years[0], years[-1]])
+        ax.set_xticks(np.arange(years[0], years[-1], 1)[::2])
+        ax.set_xticklabels(np.arange(years[0], years[-1], 1)[::2], rotation=60)
+    
+    for ax in [ax2, ax3]:
+        ax.set_xlim(right=50)
+        ax.set_xlim(left=0)
+    
+    for ax in axs:
+        ax.set_ylim(bottom=0)
+        ax.legend()
+        ax.grid()
+    
+    plt.suptitle('%s %s %d – %d\nn=%d'%(site, k, years[0], years[-1], obs[k].dropna().index.size))
+    # plt.tight_layout()
+    # plt.show()
+    
+    os.makedirs(figdir + '%s/'%site, exist_ok=True)
+    figfile = figdir + '%s/%s.hist.png'%(site, k)
+    plt.savefig(figfile)
+    plt.close()
+
+### Trim the sub-surface levels out here
+# Probably best to nan them out rather than get rid of the levels themselves for easier indexing later...
+
+# Noting that the GFS has less vertical levels than the ERA5, 
+# trim here if we are going to use index vs matched levels
+gfs_levels = [200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 
+              700, 750, 800, 850, 900, 925, 950, 975, 1000]
+
+era5_prof = era5_prof.sel(level=gfs_levels)
+
+trim_elev = era5_elev#metadata['elevation_m']
+use_min = False
+
+# If we want to use the GFS elevation we can import it here
+print('ERA5 Elevation: %dm\nMeasured Elevation: %dm'%(
+    era5_elev, metadata['elevation_m']))
+
+zmean = era5_prof['Z'].mean(dim='time')/era5_g
+zmax = era5_prof['Z'].max(dim='time')/era5_g
+zmin = era5_prof['Z'].min(dim='time')/era5_g
+
+for level in era5_prof.level.values:
+ 
+    zcompare = zmean.sel(level=level)
+    zcompare1 = zmin.sel(level=level) if use_min else zcompare
+    
+    if ((zcompare >= trim_elev)&(zcompare1 >= trim_elev)):
+        lowest_level = level
+    
+    else:
+        print('\nLowest Level Kept: %d mb\n\tMean Z: %.2f\n\tMax Z: %.2f\n\tMin Z: %.2f'%(
+            lowest_level, zmean.sel(level=lowest_level),
+            zmax.sel(level=lowest_level), zmin.sel(level=lowest_level)))
+        
+        print('\nTrim Level: %d mb\n\tMean Z: %.2f\n\tMax Z: %.2f\n\tMin Z: %.2f\n'%(
+            level, zmean.sel(level=level),
+            zmax.sel(level=level), zmin.sel(level=level)))
+        
+        break
+
+# May want to consdier leaving the levels in and using a nan-fill if this
+# proves difficult to merge into a larger dataframe later
+era5_prof = era5_prof.sel(level=slice(era5_prof.level.min(), lowest_level))
+
+# 10 levels allows stations as high as 650 mb. 
+# If higher is needed, add gfs_levels above 200 mb
+num_levels = 10
+
+era5_prof = era5_prof.sel(level=sorted(era5_prof.level[num_levels*-1:])[::-1])
+
+print(era5_prof.level)
+era5_prof['level'] = np.arange(num_levels) + 1
+
+# ### Take care of the derived variables
+# Wind Speed, Wind Direction
+for level_set in ['', '10M', '100M']:
+    
+    u = era5_prof['U%s'%level_set] 
+    v = era5_prof['V%s'%level_set]
+    
+    wspd = np.sqrt((u**2) + (v**2))
+    wdir = 90 - np.degrees(np.arctan2(-v, -u))
+    wdir = xr.where(wdir <= 0, wdir+360, wdir)
+    wdir = xr.where(((u == 0) & (v == 0)), 0, wdir)
+    
+    era5_prof['SPD%s'%level_set] = wspd.astype(np.float32)
+    era5_prof['DIR%s'%level_set] = wdir.astype(np.float32)
+
+# ### Resample to observation interval
+# From 1h or 3h ERA5 to 6, 12, 24h observations<br>
+# I think we need to manually resample here...<br>
+# to account for irregular observation intervals at some stations
+
+resample_these = {
+    'max':['Q', 'T', 'U', 'V', 'VO', 'W', 'Z', 'R', 'T2M', 'D2M', 'BLH', 'CAPE', 'MSL',
+           'SP', 'SPD', 'DIR', 'U10M', 'V10M', 'U100M', 'V100M', 'SPD10M', 'DIR10M', 'SPD100M', 'DIR100M'],
+    
+    'min':['Q', 'T', 'VO', 'Z', 'R', 'T2M', 'D2M', 'MSL', 'SP'],
+    
+    'mean':['Q', 'T', 'U', 'V', 'VO', 'W', 'Z', 'R', 'T2M', 'D2M', 'BLH', 'CAPE', 'MSL', 
+           'SP', 'SPD', 'DIR', 'U10M', 'V10M', 'U100M', 'V100M', 'SPD10M', 'DIR10M', 'SPD100M', 'DIR100M']}
+
+for interval in intervals:
+    
+    left_inclusive = True
+    right_inclusive = True
+
+    era5_resampled = []
+    
+    for i, t in enumerate(obs.index):
+
+        # Determine start, end
+        ti = t - timedelta(hours=interval)    
+        ti = ti if left_inclusive else ti + timedelta(hours=1)
+        tf = t if right_inclusive else t - timedelta(hours=1)    
+        
+        try:
+            era5_slice = era5_prof.sel(time=obs.loc[t]['precip_periods'])
+        except:
+            era5_slice = era5_prof.sel(time=slice(ti, tf))
+
+        if era5_slice.time.size > 0:
+
+            print('\rresampling %dh %d/%d (slice size %d)'%(interval, i+1, len(obs.index), era5_slice.time.size), end='')
+
+            slice_max = era5_slice[resample_these['max']].max(dim='time')
+            slice_min = era5_slice[resample_these['min']].min(dim='time')
+            slice_mean = era5_slice[resample_these['mean']].mean(dim='time')
+
+            slice_all = []
+            for oper in ['max', 'min', 'mean']:
+                slice_oper = vars()['slice_%s'%oper]
+
+                for k in slice_oper.keys():
+                    slice_oper = slice_oper.rename({k:'%s_%s'%(k, oper)})
+
+                    # Ensure that we label the observation time appropriately
+                    slice_oper['time'] = t
+
+                slice_all.append(slice_oper)
+
+            era5_resampled.append(xr.merge(slice_all))
+
+    era5_resampled = xr.concat(era5_resampled, dim='time').dropna(dim='time')
+    print()
+
+    ### Transform 2D vars (level, time) to 1D vars (time) for model input
+    isokeys = [k for k in era5_resampled if len(era5_resampled[k].shape) > 1]
+    sfckeys = [k for k in era5_resampled if len(era5_resampled[k].shape) == 1]
+
+    era5_df = []
+
+    for l in era5_resampled.level.values:
+
+        level_data = era5_resampled[isokeys].sel(level=l).drop('level')#.to_dataframe()
+        level_data = level_data.rename({k:'%s%s_%s'%(k.split('_')[0], '_%02dagl'%l, k.split('_')[-1]) for k in level_data.keys()})
+
+        era5_df.append(level_data)
+
+    era5_df = xr.merge(era5_df)
+    era5_df = xr.merge([era5_df, era5_resampled[sfckeys]])
+
+    era5_df = era5_df.to_dataframe().sort_index()
+    #era5_df = era5_df.drop_duplicates().dropna().sort_index()
+
+    droplist = [k for k in obs.keys() if (('slr%d'%interval not in k)&('swe%d'%interval not in k))]
+
+    df_merged = obs.drop(columns=droplist).rename(columns={k:k.replace('%d'%interval, '') 
+                    for k in obs.drop(columns=droplist).keys()})
+    
+    df_merged = df_merged.merge(era5_df, on='time', copy=False)
+    
+    try:
+        df_merged = df_merged.loc[np.in1d(era5_df.index, obs.index)]
+    except:
+        pass
+
+    print(interval, df_merged.shape)
+    df_merged.iloc[:10, :12]
+    
+    # df_save[interval] = df_merged.copy()
+    start, end = df_merged.index[0].year, df_merged.index[-1].year
+    df_savestr = obdir + 'combined/' + '%s_%04d_%04d.%02dh.pd'%(site, start, end, interval)
+    df_merged.to_pickle(df_savestr)
+    print('Saved:\n%s'%df_savestr)
+    del df_merged
+    gc.collect()
